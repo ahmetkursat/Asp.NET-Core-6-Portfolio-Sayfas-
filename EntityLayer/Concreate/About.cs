000c29f4690430d3ee0564f8c859925f43b9b285
Ahmet Kürşad YILDIZ <ahmetkursatyildiz_53@hotmail.com>
2023-03-28 17:19:44 +0300

    28.03.2023
    EntityFramework Eklendi ve Entityler Oluşturuldu

---
+++ EntityLayer/Concreate/About.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concreate
+{
+    public class About
+    {
+        [Key]
+        public int AboutID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+        public string Age { get; set; }
+
+        public string Phone { get; set; }
+        public string Adres { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
